526f8e326d9d9b3245fb3ece428a4b0e11b6fd73
loic-dev <loic.charrie.12@gmail.com>
2023-11-21 17:11:44 -0500

    add logo splashscreen

--- app/src/main/java/com/example/projet_prog_mobile/presentation/screens/SplashScreen.kt
+++ app/src/main/java/com/example/projet_prog_mobile/presentation/screens/SplashScreen.kt
@@ -1,11 +1,21 @@
 package com.example.projet_prog_mobile.presentation.screens
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.projet_prog_mobile.R
 import com.example.projet_prog_mobile.presentation.state.AuthState
@@ -34,10 +44,26 @@ fun SplashScreen(
         }
     }
 
-    Image(
-        painter = painterResource(id = R.drawable.ic_launcher_foreground),
-        contentDescription = "Logo"
-    )
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(colorResource(R.color.white))
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize().padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            Image(
+                painter = painterResource(id = R.drawable.logo_smartshop),
+                contentDescription = "Logo"
+
+            )
+        }
+    }
+
+
 
 
 
