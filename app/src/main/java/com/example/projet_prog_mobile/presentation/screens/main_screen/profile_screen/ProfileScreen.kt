16401a8bc968dafbb18348b24eff32272295ca6b
loic-dev <loic.charrie.12@gmail.com>
2023-12-05 19:30:25 -0500

    fix style profile screen

--- app/src/main/java/com/example/projet_prog_mobile/presentation/screens/main_screen/profile_screen/ProfileScreen.kt
+++ app/src/main/java/com/example/projet_prog_mobile/presentation/screens/main_screen/profile_screen/ProfileScreen.kt
@@ -1,10 +1,18 @@
 package com.example.projet_prog_mobile.presentation.screens.main_screen.profile_screen
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.example.projet_prog_mobile.R
+import com.example.projet_prog_mobile.presentation.components.AuthInputButton
 import com.example.projet_prog_mobile.presentation.components.NavDestinationHelper
 import com.example.projet_prog_mobile.presentation.containers.ProfileContainer
 import com.example.projet_prog_mobile.presentation.viewModel.ProfileViewModel
@@ -27,10 +35,25 @@ fun ProfileScreen(modifier: Modifier,
         profileViewModel.onLoad()
     }
 
-    ProfileContainer(isLoading = profileViewModel.profileState.isLoading,
-        firstname = profileViewModel.profileState.firstname ,
-        lastname = profileViewModel.profileState.lastname,
-        email = profileViewModel.profileState.email,
-        modifier = modifier,
-        onLogoutButtonClick = profileViewModel::onDisconnect)
+    Column(modifier=modifier) {
+        Text(
+            text = "PROFILE",
+            style = MaterialTheme.typography.titleLarge.copy(
+                fontWeight = FontWeight.Bold,
+                color = colorResource(id = R.color.light_black)
+            )
+        )
+        ProfileContainer(
+            firstname = profileViewModel.profileState.firstname ,
+            lastname = profileViewModel.profileState.lastname,
+            email = profileViewModel.profileState.email,
+            modifier = Modifier)
+        AuthInputButton(
+            modifier=Modifier,
+            buttonEnabled= { true },
+            isLoading= { profileViewModel.profileState.isLoading },
+            onButtonClick= {profileViewModel.onDisconnect()},
+            textButton= stringResource(R.string.profile_logout)
+        )
+    }
 }
